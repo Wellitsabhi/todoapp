75cf72be174c52274adf07a078ce9d26e27604aa
Abhishek Singh <99867024+Wellitsabhi@users.noreply.github.com>
2024-05-26 09:56:05 +0530

    Update api.js for live backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `http://localhost:${import.meta.env.VITE_PORT}`
+    // baseURL: `http://localhost:${import.meta.env.VITE_PORT}`
+    baseURL: 'https://todoapp-1-18ru.onrender.com'
 });
 
 export default api;
